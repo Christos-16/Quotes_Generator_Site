ee41ee2bdc38b4330356fdf3d03de7de08baaddf
Christos-16 <anastasiouxrhstos@gmail.com>
2022-12-07 20:11:24 +0200

    half project

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote(){
+    //Pick arandom quote from apiQuotes array
+    const quote = apiQuotes [Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+
+async function getQuotes () {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote
+
+    } catch (error) {
+        //Catch error here
+
+    }
+}
+
+//On Load
+getQuotes();
